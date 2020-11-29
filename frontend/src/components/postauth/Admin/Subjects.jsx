8a371d4e937886432511844c5c22d6c50a6e99c1
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-11-29 10:31:39 +0530

    Delete subjects

--- frontend/src/components/postauth/Admin/Subjects.jsx
+++ frontend/src/components/postauth/Admin/Subjects.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import {
-    Card,
-    CardHeader,
-    Grid,
-    Button,
-    List,
-		ListItem,
-		ListItemAvatar,
-		Typography,
-		TextField,
-		ListItemText,
-		Avatar
+	Card,
+	CardHeader,
+	Grid,
+	Button,
+	List,
+	ListItem,
+	ListItemAvatar,
+	Typography,
+	TextField,
+	ListItemText,
+	Avatar,
+	ListItemSecondaryAction,
+	IconButton
 } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { useSnackbar } from "notistack";
 import Axios from 'axios';
 
@@ -19,211 +22,234 @@ export default function Subjects() {
 	const { enqueueSnackbar } = useSnackbar();
 
 	const [allSubjects, setAllSubjects] = useState([]);
-  const [addSubjects, setAddSubjects] = useState([{
-    subId: '',
-    subName: '',
-    year: 2
+	const [addSubjects, setAddSubjects] = useState([{
+		subId: '',
+		subName: '',
+		year: 2
 	}]);
-	
+
 	const handleAddSubject = () => {
-    setAddSubjects([...addSubjects, {subId: '', subName: '', year: ''}])
-  }
+		setAddSubjects([...addSubjects, { subId: '', subName: '', year: '' }])
+	}
 
-  const handleRemoveSubject = () => {
-    const newAddSubjects = [...addSubjects];
-    newAddSubjects.pop();
-    setAddSubjects(newAddSubjects);
-  }
+	const handleRemoveSubject = () => {
+		const newAddSubjects = [...addSubjects];
+		newAddSubjects.pop();
+		setAddSubjects(newAddSubjects);
+	}
 
-  const handleChange = (event) => {
-    let et = event.target
-    let property = event.target.name.split(' ')[0]
-    let index = parseInt(event.target.name.split(' ')[1])
-    let value = et.value
-    let newAddSubjects = [...addSubjects]
-    newAddSubjects[index] = {...newAddSubjects[index], [property]: value}
-    setAddSubjects(newAddSubjects);
-  }
+	const handleChange = (event) => {
+		let et = event.target
+		let property = event.target.name.split(' ')[0]
+		let index = parseInt(event.target.name.split(' ')[1])
+		let value = et.value
+		let newAddSubjects = [...addSubjects]
+		newAddSubjects[index] = { ...newAddSubjects[index], [property]: value }
+		setAddSubjects(newAddSubjects);
+	}
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    addSubjects.forEach((subject) => {
-      if(!subject.subId || !subject.subName) {
-        enqueueSnackbar('Please fill all fields', {variant: 'error'});
-        return;
-      }
-    })
-    if(addSubjects.length === 1) {
-      if(!addSubjects[0].subId || !addSubjects[0].subName) {
-        enqueueSnackbar('Please fill all fields', {variant: 'error'});
-        return;
-      }
-      Axios.post(
-        'http://localhost:8000/api/subject/add',
-        addSubjects[0],
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${sessionStorage.getItem("usertoken")}`,
-          }
-        }
-      )
-      .then(res => {
-          enqueueSnackbar('Added subject', {variant:'success'})
-          let newAllSubjects = [...allSubjects]
-          newAllSubjects.push(addSubjects[0])
-          setAllSubjects(newAllSubjects);
-      })
-      .catch(err => enqueueSnackbar('This subject already exists', {variant: 'error'}))
-    } else {
-      Axios.post(
-        'http://localhost:8000/api/subject/add/multiple',
-        {
-          subjects: addSubjects
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${sessionStorage.getItem("usertoken")}`,
-          }
-        }
-      )
-      .then(res => {
-        enqueueSnackbar('Added subject', {variant:'success'})
-        let newAllSubjects = [...allSubjects]
-        addSubjects.forEach(subject => newAllSubjects.push(subject));
-        setAllSubjects(newAllSubjects);
-      })
-      .catch(err => enqueueSnackbar('Invalid', {variant: 'error'}))
-    }
-  }
+	const handleSubmit = event => {
+		event.preventDefault();
+		addSubjects.forEach((subject) => {
+			if (!subject.subId || !subject.subName) {
+				enqueueSnackbar('Please fill all fields', { variant: 'error' });
+				return;
+			}
+		})
+		if (addSubjects.length === 1) {
+			if (!addSubjects[0].subId || !addSubjects[0].subName) {
+				enqueueSnackbar('Please fill all fields', { variant: 'error' });
+				return;
+			}
+			Axios.post(
+				'http://localhost:8000/api/subject/add',
+				addSubjects[0],
+				{
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${sessionStorage.getItem("usertoken")}`,
+					}
+				}
+			)
+				.then(res => {
+					enqueueSnackbar('Added subject', { variant: 'success' })
+					let newAllSubjects = [...allSubjects]
+					newAllSubjects.push(addSubjects[0])
+					setAllSubjects(newAllSubjects);
+				})
+				.catch(err => enqueueSnackbar('This subject already exists', { variant: 'error' }))
+		} else {
+			Axios.post(
+				'http://localhost:8000/api/subject/add/multiple',
+				{
+					subjects: addSubjects
+				},
+				{
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${sessionStorage.getItem("usertoken")}`,
+					}
+				}
+			)
+				.then(res => {
+					enqueueSnackbar('Added subject', { variant: 'success' })
+					let newAllSubjects = [...allSubjects]
+					addSubjects.forEach(subject => newAllSubjects.push(subject));
+					setAllSubjects(newAllSubjects);
+				})
+				.catch(err => enqueueSnackbar('Invalid', { variant: 'error' }))
+		}
+	}
 
-  useEffect(() => {
-    Axios.get("http://localhost:8000/api/subject/all", {
+	const handleDeleteSubject = (subId) => {
+		Axios.delete(`http://localhost:8000/api/subject/delete/${subId}`, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${sessionStorage.getItem("usertoken")}`,
       },
-    }).then((res) => setAllSubjects(res.data.data));
-  }, [enqueueSnackbar]);
+    }).then((res) => {
+      let newAllSubjects = [...allSubjects];
+      newAllSubjects = allSubjects.filter((subject) => subject.subId !== subId);
+      setAllSubjects(newAllSubjects);
+      enqueueSnackbar("Deleted!", { variant: "success" });
+    });
+	}
+
+	useEffect(() => {
+		Axios.get("http://localhost:8000/api/subject/all", {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${sessionStorage.getItem("usertoken")}`,
+			},
+		}).then((res) => setAllSubjects(res.data.data));
+	}, [enqueueSnackbar]);
 
 
 	return (
 		<Grid item>
-		<Card>
-			<CardHeader
-				title="Add Subjects"
-				titleTypographyProps={{ variant: "h3" }}
-			/>
-			<form onSubmit={handleSubmit}>
-			{
-				addSubjects.map((subject, index) => (
-					<Grid container item spacing={2} style={{padding:'1%'}}>
-						<Grid item>
-						<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						defaultValue={subject.subName}
-						label="Enter Subject Name"
-						name={"subName " + index}
-						autoComplete="subName"
-						onChange={handleChange}
-						autoFocus
-					/>
-					</Grid>
-					<Grid item>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							defaultValue={subject.subId}
-							label="Enter Subject ID"
-							name={"subId " + index}
-							onChange={handleChange}
-							autoComplete="subID"
-							/>
-					</Grid>
-					<Grid item>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							defaultValue={subject.year}
-							label="Enter year"
-							name={"year " + index}
-							onChange={handleChange}
-							autoComplete="year"
-							/>
-						</Grid>              
-					</Grid>
-				))
-			}            
-			<Button 
-				color="primary" 
-				variant="contained" 
-				onClick={handleSubmit}
-				style={{margin: '0% 0% 2% 2%'}}
-			>
-					Submit
-			</Button>
-			<Button 
-				color="secondary" 
-				variant="contained" 
-				onClick={handleAddSubject}
-				style={{margin: '0% 0% 2% 2%'}}
-			>
-					Add More Subjects
-			</Button>          
-			{
-				addSubjects.length > 1 &&
-				<Button 
-					color="secondary" 
-					variant="contained" 
-					onClick={handleRemoveSubject}
-					style={{margin: '0% 0% 2% 2%'}}
-				>
-						Remove Subject
-				</Button>
-			}      
-			</form>
-		</Card>
 			<Card>
-			<CardHeader
-				title="Subjects Added"
-				titleTypographyProps={{ variant: "h3" }}
-			/>
-			<List dense>
-				{allSubjects.map((subject, index) => (
-					<ListItem id={subject.subId} key={index}>
-						<ListItemAvatar>
-							<Avatar>{subject.subName.charAt(0)}</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary={
-								<Typography variant="h6" color="textPrimary">
-									{subject.subName}
-								</Typography>
-							}
-							secondary={
-								<React.Fragment>
-									<Typography variant="body1" color="textPrimary">
-										{subject.subId}
+				<CardHeader
+					title="Add Subjects"
+					titleTypographyProps={{ variant: "h3" }}
+				/>
+				<form onSubmit={handleSubmit}>
+					{
+						addSubjects.map((subject, index) => (
+							<Grid container item spacing={2} style={{ padding: '1%' }}>
+								<Grid item>
+									<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										defaultValue={subject.subName}
+										label="Enter Subject Name"
+										name={"subName " + index}
+										autoComplete="subName"
+										onChange={handleChange}
+										autoFocus
+									/>
+								</Grid>
+								<Grid item>
+									<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										defaultValue={subject.subId}
+										label="Enter Subject ID"
+										name={"subId " + index}
+										onChange={handleChange}
+										autoComplete="subID"
+									/>
+								</Grid>
+								<Grid item>
+									<TextField
+										variant="outlined"
+										margin="normal"
+										required
+										fullWidth
+										defaultValue={subject.year}
+										label="Enter year"
+										name={"year " + index}
+										onChange={handleChange}
+										autoComplete="year"
+									/>
+								</Grid>
+							</Grid>
+						))
+					}
+					<Button
+						color="primary"
+						variant="contained"
+						onClick={handleSubmit}
+						style={{ margin: '0% 0% 2% 1%' }}
+					>
+						Submit
+			</Button>
+					<Button
+						color="secondary"
+						variant="contained"
+						onClick={handleAddSubject}
+						style={{ margin: '0% 0% 2% 2%' }}
+					>
+						Add More Subjects
+			</Button>
+					{
+						addSubjects.length > 1 &&
+						<Button
+							color="secondary"
+							variant="contained"
+							onClick={handleRemoveSubject}
+							style={{ margin: '0% 0% 2% 2%' }}
+						>
+							Remove Subject
+				</Button>
+					}
+				</form>
+			</Card>
+			<Card>
+				<CardHeader
+					title="Subjects Added"
+					titleTypographyProps={{ variant: "h3" }}
+				/>
+				<List dense>
+					{allSubjects.map((subject, index) => (
+						<ListItem id={subject.subId} key={index}>
+							<ListItemAvatar>
+								<Avatar>{subject.subName.charAt(0)}</Avatar>
+							</ListItemAvatar>
+							<ListItemText
+								primary={
+									<Typography variant="h6" color="textPrimary">
+										{subject.subName}
 									</Typography>
-									<Typography variant="body2" color="textSecondary">
-										Year : {subject.year}
-									</Typography>
-								</React.Fragment>
-							}
-						/>
-					</ListItem>
-				))}
-			</List>
-		</Card>
-	</Grid>
+								}
+								secondary={
+									<React.Fragment>
+										<Typography variant="body1" color="textPrimary">
+											{subject.subId}
+										</Typography>
+										<Typography variant="body2" color="textSecondary">
+											Year : {subject.year}
+										</Typography>
+									</React.Fragment>
+								}
+							/>
+							<ListItemSecondaryAction>
+								<IconButton
+									edge="end"
+									aria-label="delete"
+									onClick={() => handleDeleteSubject(subject.subId)}
+								>
+									<DeleteIcon />
+								</IconButton>
+							</ListItemSecondaryAction>
+						</ListItem>
+					))}
+				</List>
+			</Card>
+		</Grid>
 	)
 
 }
